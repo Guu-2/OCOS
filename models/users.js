41d0131cd1b5216efc17ba7aad405dde24ff08ed
anfeco33 <131100619+anfeco33@users.noreply.github.com>
2024-04-08 11:58:12 +0700

    fix(schema): add attributes

--- models/users.js
+++ models/users.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const crypto = require('crypto')
 
 const userSchema = new Schema({
     username: { type: String, required: true },
-    password: { type: String, required: true },
+    password: { type: String, required: false },
     email: { type: String, required: true , unique: true},
+    googleId: {type: String, required: false},
     fullName: { type: String, required: true },
     role: { type: String, enum: ['admin', 'student','instructor'], required: true },
     access: [
@@ -22,7 +24,20 @@ const userSchema = new Schema({
     lastLogin: { type: String , required: true},
     createdAt: { type: String, default: new Date().toUTCString()},
     lock: { type: Boolean, default: false},
+    passwordChangedAt: { type: Date},
+    password_reset_token: { type: String},
+    password_reset_expires: { type: Date},
 });
+
+userSchema.methods.createResetPasswordToken = function() {
+  const resetToken = crypto.randomBytes(32).toString('hex')
+
+  this.password_reset_token = crypto.createHash('sha256').update(resetToken).digest('hex')
+  this.password_reset_expires = Date.now() + 10 * 60 * 1000
+
+  console.log({ resetToken }, this.password_reset_token)
+  return resetToken
+}
 
 const User = mongoose.model('User', userSchema);
 
